dcedafbc16a41dfe8c3cccb5852645ca9bde2517
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-06-29 15:23:32 -0400

    summary (tasty/nasty) added

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,7 +3,6 @@ $(function () {
     $(".like").click(function () {
         var input = $(this).find('.qty1');
         input.val(parseInt(input.val())+ 1);
-
     });
     $(".dislike").click(function () {
         var input = $(this).find('.qty2');
